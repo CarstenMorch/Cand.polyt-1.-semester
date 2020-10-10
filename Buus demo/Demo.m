6a33628e3b3a3618f7c4c7bf9b90e20cc9cc8ff4
Andreas Buus Andersen <aa17@student.aau.dk>
2020-10-10 21:24:58 +0200

    Trin 1 til 5 er tilføjet i demo
    Intet kode er skrevet ret endnu - prøver bare at forstå

--- Buus demo/Demo.m
+++ Buus demo/Demo.m
@@ -42,4 +42,93 @@ plot(StimLevelsFine,Fit,'g-','linewidth',2);
 hold on;
 plot(StimLevels, PropCorrectData,'k.','markersize',40);
 
+%% Look up tabel  p(success if lampda given x)
+clc 
+clear 
+disp(' >> Look up tabel << '); 
+
+grain = 201; 
+PF = @PAL_Gumbel;
+
+%Stimulus values the method can select from
+PM.stimRange = (linspace(PF([0 1 0 0],.1,'inverse'),PF([0 1 0 0],.9999,'inverse'),21));
+
+%Define parameter ranges to be included in posterior
+PM.priorAlphaRange = linspace(PF([0 1 0 0],.1,'inverse'),PF([0 1 0 0],.9999,'inverse'),grain);
+PM.priorBetaRange =  linspace(log10(.0625),log10(16),grain); %OBS ANGIVET I LOG!
+PM.priorGammaRange = 0.5;  
+PM.priorLambdaRange = .02; 
+PM.gammaEQlambda = 0; 
+
+%parameter to simulate observer
+paramsGen = [0, 1, .5, .02]; 
+
+%PDF :
+    PM.prior = ones(length(PM.priorAlphaRange),length(PM.priorBetaRange),length(PM.priorGammaRange),length(PM.priorLambdaRange));
+    PM.prior = PM.prior./numel(PM.prior); % number of elements
+    PM.pdf = PM.prior; 
+
+ 
+%LOOK UP TABEL   : 
+    for a = 1:length(PM.priorAlphaRange)
+        for b = 1:length(PM.priorBetaRange) %OBS. Udregnes ikke i log!
+            for g = 1:length(PM.priorGammaRange)
+                for L = 1:length(PM.priorLambdaRange) 
+                    for sLevel = 1:length(PM.stimRange)
+                        %sandsyndligheden for at for korrekt response ved en given parameter sammensætning og intensitet.  
+                        LUT(a,b,g,L,sLevel) = PF([PM.priorAlphaRange(a), 10.^PM.priorBetaRange(b), PM.priorGammaRange(g), PM.priorLambdaRange(L)], PM.stimRange(sLevel));
+                    end
+                end
+            end
+        end 
+    end     
+    
+%% Trin 1 og 2 (posterior probability)
+    pdf5D = repmat(PM.pdf, [1 1 1 1 length(PM.stimRange)]); % 201 201 --> 201 201 1 1 21 
+    PosteriorTplus1givenSuccess = pdf5D.*LUT; % p(succes/lambda, x)
+    PosteriorTplus1givenFailure = pdf5D-PosteriorTplus1givenSuccess;
+   
+    size(PosteriorTplus1givenSuccess)
+    
+    Denominator = squeeze(sum(sum(sum(sum(PosteriorTplus1givenSuccess,1),2),3),4)); %sandsynligheden for x ift all parameter, return the 1,1,1,1,21 --> 21,1
+    Denominator = repmat(Denominator, [1 size(pdf5D,1) size(pdf5D,2) size(pdf5D,3) size(pdf5D,4)]); %[21,1] --> [21, 201, 201]    
+    Denominator = permute(Denominator, [2 3 4 5 1]); %[21, 201, 201] --> [201 201 1 1 21]
+    
+% Sandsynligheden for alpha og beta ved intensitet x divideret med 
+% sandsynligheden for den summeret sandsynlighed af alle alpha og beta
+% værdier ved intensitet x. 
+% p_t (lambda|x,r)
+    PosteriorTplus1givenSuccess = PosteriorTplus1givenSuccess./Denominator; 
+    
+    Denominator = squeeze(sum(sum(sum(sum(PosteriorTplus1givenFailure,1),2),3),4));
+    Denominator = repmat(Denominator, [1 size(pdf5D,1) size(pdf5D,2) size(pdf5D,3) size(pdf5D,4)]);
+    Denominator = permute(Denominator, [2 3 4 5 1]);
+    PosteriorTplus1givenFailure = PosteriorTplus1givenFailure./Denominator;
+    
+    %pt(success|x)
+    pSuccessGivenx = sum(sum(sum(sum(pdf5D.*LUT,1),2),3),4); 
+    
+
+%% Trin 3,4 and 5 (ENTROPY)
+    %success
+    EntropyS = PosteriorTplus1givenSuccess.*log(PosteriorTplus1givenSuccess);
+    EntropyS(isnan(EntropyS)) = 0;          %effectively defines 0.*log(0) to equal 0.
+    EntropyS = sum(EntropyS,'all');
+    EntropyS = -EntropyS;
+    
+    %Failure
+    EntropyF = PosteriorTplus1givenFailure.*log(PosteriorTplus1givenFailure);
+    EntropyF(isnan(EntropyF)) = 0;          %effectively defines 0.*log(0) to equal 0.
+    EntropyF = sum(EntropyF,'all');
+    EntropyF = -EntropyF;
+    
+% E[Ht(x)]: 
+    Entropy = EntropyS.*pSuccessGivenx + EntropyF.*(1-pSuccessGivenx); % [1,1,1,1,21]
+    
+% Find minimum: 
+    [minEntropy, newIntensityLevel] = min(squeeze(Entropy)); % minentropy: the value, i; index position. 
+    PM.xCurrent = PM.stimRange(newIntensityLevel);
+    PM.x(1) = PM.xCurrent; %The first stimulation value!! what up!
+
+    response = rand(1) < PF(paramsGen, PM.xCurrent);    %simulate observer
 
